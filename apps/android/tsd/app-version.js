3d637aa4c117882cf409ea630381de0a88c9539a
naskek <semen1988@gmail.com>
2026-05-22 14:33:51 +0300

    chore(pwa): bump tsd app version to v16

--- apps/android/tsd/app-version.js
+++ apps/android/tsd/app-version.js
@@ -1,9 +1,9 @@
-/**
- * Версия shell-кэша TSD PWA. Увеличивайте при каждом деплое, если менялись
- * index.html, service-worker.js, app.js, styles.css, storage.js или scanner.js.
+﻿/**
+ * Р’РµСЂСЃРёСЏ shell-РєСЌС€Р° TSD PWA. РЈРІРµР»РёС‡РёРІР°Р№С‚Рµ РїСЂРё РєР°Р¶РґРѕРј РґРµРїР»РѕРµ, РµСЃР»Рё РјРµРЅСЏР»РёСЃСЊ
+ * index.html, service-worker.js, app.js, styles.css, storage.js РёР»Рё scanner.js.
  */
 (function (root) {
-  var version = "15";
+  var version = "16";
   var cacheName = "flowstock-tsd-v" + version;
   root.TSD_PWA_VERSION = version;
   root.TSD_CACHE_NAME = cacheName;
